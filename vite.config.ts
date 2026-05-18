6b365d595c2121b91f8743ad680b80450dc4dddd
Brian Bennett <42482128+bbenn88@users.noreply.github.com>
2026-05-18 16:06:00 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: '/workout-plan/'
+  base: '/Workout_Plan/'
 })
